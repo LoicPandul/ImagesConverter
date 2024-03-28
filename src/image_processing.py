9bcc738d3ac95a4437f6f61e8257b677e3f57241
Loïc Morel <137194052+LoicPandul@users.noreply.github.com>
2024-03-28 07:50:25 +0100

    traduction eng

--- src/image_processing.py
+++ src/image_processing.py
@@ -8,11 +8,11 @@ def is_same_format(target_format, image_path):
 def convert_to_jpeg(image_paths, gui_instance):
     for image_path in image_paths:
         if not os.path.isfile(image_path):
-            print(f"Le fichier {image_path} n'existe pas.")
+            print(f"The file {image_path} does not exist.")
             continue
         
         if is_same_format('jpeg', image_path):
-            print(f"Le fichier {image_path} est déjà au format JPEG, aucune conversion nécessaire.")
+            print(f"The file {image_path} is already in JPEG format, no conversion necessary.")
             continue
 
         with Image.open(image_path) as image:
@@ -20,44 +20,45 @@ def convert_to_jpeg(image_paths, gui_instance):
                 image = image.convert("RGB")
             image_output_path = os.path.splitext(image_path)[0] + '.jpeg'
             image.save(image_output_path, 'JPEG')
-            print(f"{image_path} a été converti en {image_output_path}.")
+            print(f"{image_path} has been converted to {image_output_path}.")
 
         os.remove(image_path)
-        print(f"{image_path} a été supprimé.")
+        print(f"{image_path} has been deleted.")
 
 def convert_to_webp(image_paths, gui_instance):
     for image_path in image_paths:
         if not os.path.isfile(image_path):
-            print(f"Le fichier {image_path} n'existe pas.")
+            print(f"The file {image_path} does not exist.")
             continue
         
         if is_same_format('webp', image_path):
-            print(f"Le fichier {image_path} est déjà au format WEBP, aucune conversion nécessaire.")
+            print(f"The file {image_path} is already in WEBP format, no conversion necessary.")
             continue
 
         with Image.open(image_path) as image:
             image_output_path = os.path.splitext(image_path)[0] + '.webp'
             image.save(image_output_path, 'WEBP')
-            print(f"{image_path} a été converti en {image_output_path}.")
+            print(f"{image_path} has been converted to {image_output_path}.")
 
         os.remove(image_path)
-        print(f"{image_path} a été supprimé.")
+        print(f"{image_path} has been deleted.")
 
 def convert_to_png(image_paths, gui_instance):
     for image_path in image_paths:
         if not os.path.isfile(image_path):
-            print(f"Le fichier {image_path} n'existe pas.")
+            print(f"The file {image_path} does not exist.")
             continue
 
         if is_same_format('png', image_path):
-            print(f"Le fichier {image_path} est déjà au format PNG, aucune conversion nécessaire.")
+            print(f"The file {image_path} is already in PNG format, no conversion necessary.")
             continue
 
         with Image.open(image_path) as image:
             image_output_path = os.path.splitext(image_path)[0] + '.png'
             image.save(image_output_path, 'PNG')
-            print(f"{image_path} a été converti en {image_output_path}.")
+            print(f"{image_path} has been converted to {image_output_path}.")
 
         os.remove(image_path)
-        print(f"{image_path} a été supprimé.")
+        print(f"{image_path} has been deleted.")
+
 
