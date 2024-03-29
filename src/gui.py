35bd8a9e9e68e22dc31b5dc0772a3ce53d480bf6
pythcoiner <pythcoiner@proton.me>
2024-03-29 01:32:48 +0100

    pep8

--- src/gui.py
+++ src/gui.py
@@ -1,10 +1,12 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QTextEdit, QLabel, QGraphicsView, QGraphicsScene, QGraphicsPixmapItem, QGraphicsTextItem
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QTextEdit, \
+    QGraphicsView, QGraphicsScene, QGraphicsPixmapItem, QGraphicsTextItem
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QDragEnterEvent, QDropEvent, QPixmap, QIcon
 import sys
 import os
 
 from .image_processing import convert_to_jpeg, convert_to_webp, convert_to_png, clean_metadata
+
 
 class DragDropLabel(QGraphicsView):
     def __init__(self, imageConverterGUI=None):
@@ -25,23 +27,22 @@ class DragDropLabel(QGraphicsView):
         
         self.setAcceptDrops(True)
         self.setStyleSheet("border: 1px dashed #ccc;")
-
+        
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-
-
+    
     def dragEnterEvent(self, event: QDragEnterEvent):
         if event.mimeData().hasUrls():
             event.acceptProposedAction()
         else:
             super().dragEnterEvent(event)
-
+    
     def dragMoveEvent(self, event: QDragEnterEvent):
         if event.mimeData().hasUrls():
             event.acceptProposedAction()
         else:
             super().dragMoveEvent(event)
-
+    
     def dropEvent(self, event: QDropEvent):
         files = [url.toLocalFile() for url in event.mimeData().urls()]
         for file in files:
@@ -50,52 +51,53 @@ class DragDropLabel(QGraphicsView):
                 self.imageConverterGUI.convert_image(file)
         event.acceptProposedAction()
 
+
 class ImageConverterGUI(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Image Converter")
         self.setFixedSize(1000, 1000)
-
+        
         icon = QIcon('./assets/icone.ico')
-
+        
         self.setWindowIcon(icon)
-
+        
         self.conversion_mode = None
-
+        
         layout = QVBoxLayout()
-
+        
         self.drop_label = DragDropLabel(self)
         layout.addWidget(self.drop_label)
-
+        
         self.btn_to_jpeg = QPushButton("Convert to JPEG")
         self.btn_to_jpeg.clicked.connect(lambda: self.set_conversion_mode_and_clear_message('jpeg'))
         layout.addWidget(self.btn_to_jpeg)
-
+        
         self.btn_to_webp = QPushButton("Convert to WEBP")
         self.btn_to_webp.clicked.connect(lambda: self.set_conversion_mode_and_clear_message('webp'))
         layout.addWidget(self.btn_to_webp)
-
+        
         self.btn_to_png = QPushButton("Convert to PNG")
         self.btn_to_png.clicked.connect(lambda: self.set_conversion_mode_and_clear_message('png'))
         layout.addWidget(self.btn_to_png)
-
+        
         self.message_terminal = QTextEdit()
         self.message_terminal.setReadOnly(True)
         layout.addWidget(self.message_terminal)
-
+        
         central_widget = QWidget()
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
-
+        
         self.append_message("Select a conversion target by clicking a button to start.")
-
+    
     def append_message(self, message):
         self.message_terminal.append(message)
-
+    
     def set_conversion_mode_and_clear_message(self, mode):
-        self.message_terminal.clear() 
+        self.message_terminal.clear()
         self.set_conversion_mode(mode)
-
+    
     def set_conversion_mode(self, mode):
         self.conversion_mode = mode
         self.reset_button_styles()
@@ -106,31 +108,31 @@ class ImageConverterGUI(QMainWindow):
         elif mode == 'png':
             self.btn_to_png.setStyleSheet("background-color: #F2B807; color: black;")
         self.append_message(f"Mode : to {mode}")
-
+    
     def reset_button_styles(self):
         self.btn_to_jpeg.setStyleSheet("")
         self.btn_to_webp.setStyleSheet("")
         self.btn_to_png.setStyleSheet("")
-
+    
     def convert_image(self, image_path):
-            file_name = os.path.basename(image_path)
-            try:
-                if self.conversion_mode == 'jpeg':
-                    convert_to_jpeg([image_path], self)
-                elif self.conversion_mode == 'webp':
-                    convert_to_webp([image_path], self)
-                elif self.conversion_mode == 'png':
-                    convert_to_png([image_path], self)
+        file_name = os.path.basename(image_path)
+        try:
+            if self.conversion_mode == 'jpeg':
+                convert_to_jpeg([image_path], self)
+            elif self.conversion_mode == 'webp':
+                convert_to_webp([image_path], self)
+            elif self.conversion_mode == 'png':
+                convert_to_png([image_path], self)
+            
+            clean_metadata([image_path])
+            
+            self.append_message(f"  - {file_name} has been converted to {self.conversion_mode.upper()}.")
+        except Exception as e:
+            self.append_message(f"An error occurred while converting {file_name}: {str(e)}")
 
-                clean_metadata([image_path])
-
-                self.append_message(f"  - {file_name} has been converted to {self.conversion_mode.upper()}.")
-            except Exception as e:
-                self.append_message(f"An error occurred while converting {file_name}: {str(e)}")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = ImageConverterGUI()
     window.show()
     sys.exit(app.exec_())
-
--- src/image_processing.py
+++ src/image_processing.py
@@ -1,26 +1,29 @@
 from PIL import Image
 import os
 
+
 def is_same_format(target_format, image_path):
     _, ext = os.path.splitext(image_path)
     return ext.lower() == f'.{target_format.lower()}'
+
 
 def convert_to_jpeg(image_paths, gui_instance):
     for image_path in image_paths:
         try:
             file_name = os.path.basename(image_path)
             if not os.path.isfile(image_path) or is_same_format('jpeg', image_path):
-                continue 
-
+                continue
+            
             with Image.open(image_path) as image:
                 if image.mode in ("RGBA", "LA"):
                     image = image.convert("RGB")
                 image_output_path = os.path.splitext(image_path)[0] + '.jpeg'
                 image.save(image_output_path, 'JPEG')
-
+            
             os.remove(image_path)
         except Exception as e:
             gui_instance.append_message(f"Échec de la conversion de {file_name} en JPEG. Erreur : {e}")
+
 
 def convert_to_webp(image_paths, gui_instance):
     for image_path in image_paths:
@@ -28,14 +31,15 @@ def convert_to_webp(image_paths, gui_instance):
             file_name = os.path.basename(image_path)
             if not os.path.isfile(image_path) or is_same_format('webp', image_path):
                 continue
-
+            
             with Image.open(image_path) as image:
                 image_output_path = os.path.splitext(image_path)[0] + '.webp'
                 image.save(image_output_path, 'WEBP')
-
+            
             os.remove(image_path)
         except Exception as e:
             gui_instance.append_message(f"Échec de la conversion de {file_name} en WEBP. Erreur : {e}")
+
 
 def convert_to_png(image_paths, gui_instance):
     for image_path in image_paths:
@@ -43,14 +47,15 @@ def convert_to_png(image_paths, gui_instance):
             file_name = os.path.basename(image_path)
             if not os.path.isfile(image_path) or is_same_format('png', image_path):
                 continue
-
+            
             with Image.open(image_path) as image:
                 image_output_path = os.path.splitext(image_path)[0] + '.png'
                 image.save(image_output_path, 'PNG')
-
+            
             os.remove(image_path)
         except Exception as e:
             gui_instance.append_message(f"Échec de la conversion de {file_name} en PNG. Erreur : {e}")
+
 
 def clean_metadata(image_paths):
     for image_path in image_paths:
