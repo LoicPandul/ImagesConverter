157a2c19b999a4abf7122ebf748287012ec53c94
Loïc Morel <137194052+LoicPandul@users.noreply.github.com>
2024-04-01 10:39:00 +0200

    gestion transparence

--- src/image_processing.py
+++ src/image_processing.py
@@ -1,11 +1,9 @@
 from PIL import Image
 import os
 
-
 def is_same_format(target_format, image_path):
     _, ext = os.path.splitext(image_path)
     return ext.lower() == f'.{target_format.lower()}'
-
 
 def convert_to(image_format: str, image_paths, gui_instance):
     image_format = image_format.lower()
@@ -20,20 +18,27 @@ def convert_to(image_format: str, image_paths, gui_instance):
                 gui_instance.append_message(f"{image_path} n'est pas un fichier valide!")
                 continue
             elif is_same_format(image_format, image_path):
-                gui_instance.append_message(f"{image_path} est deja au format {image_format}")
+                gui_instance.append_message(f"{image_path} est déjà au format {image_format}")
                 continue
             
             with Image.open(image_path) as image:
-                if image.mode in ("RGBA", "LA"):
-                    image = image.convert("RGB")
                 image_output_path = os.path.splitext(image_path)[0] + '.' + image_format
-                image.save(image_output_path, image_format.upper())
-                gui_instance.append_message(f"  - {file_name} has been converted to {image_format.upper()}.")
-            
-            os.remove(image_path)
+                
+                if image_format == 'webp':
+                    if image.mode in ("RGBA", "LA"):
+                        image.save(image_output_path, image_format.upper(), quality=90, lossless=True)
+                    else:
+                        image.save(image_output_path, image_format.upper(), quality=90)
+                else:
+                    if image.mode in ("RGBA", "LA") and image_format != 'png':
+                        image = image.convert("RGB")
+                    image.save(image_output_path, image_format.upper())
+                
+                gui_instance.append_message(f"  - {file_name} a été converti en {image_format.upper()}.")
+                
+                os.remove(image_path)
         except Exception as e:
             gui_instance.append_message(f"Échec de la conversion de {file_name} en {image_format}. Erreur : {e}")
-
 
 def clean_metadata(image_paths, gui_instance):
     for image_path in image_paths:
