4b04379cdece7a55260e9fc6a4bf407435541b77
Loïc Morel <137194052+LoicPandul@users.noreply.github.com>
2024-04-05 02:02:13 +0200

    Update image_processing.py

--- src/image_processing.py
+++ src/image_processing.py
@@ -8,17 +8,17 @@ def is_same_format(target_format, image_path):
 def convert_to(image_format: str, image_paths, gui_instance):
     image_format = image_format.lower()
     if image_format not in ["jpeg", "png", "webp"]:
-        gui_instance.append_message(f"{image_format} n'est pas un format valide!")
+        gui_instance.append_message(f"{image_format} is not a valid format!")
         return
     
     for image_path in image_paths:
         file_name = os.path.basename(image_path)
         try:
             if not os.path.isfile(image_path):
-                gui_instance.append_message(f"{image_path} n'est pas un fichier valide!")
+                gui_instance.append_message(f"{image_path} is not a valid file!")
                 continue
             elif is_same_format(image_format, image_path):
-                gui_instance.append_message(f"{image_path} est déjà au format {image_format}")
+                gui_instance.append_message(f"{image_path} is already in the format {image_format}")
                 continue
             
             with Image.open(image_path) as image:
@@ -34,11 +34,11 @@ def convert_to(image_format: str, image_paths, gui_instance):
                         image = image.convert("RGB")
                     image.save(image_output_path, image_format.upper())
                 
-                gui_instance.append_message(f"  - {file_name} a été converti en {image_format.upper()}.")
+                gui_instance.append_message(f"  - {file_name} has been converted to {image_format.upper()}.")
                 
                 os.remove(image_path)
         except Exception as e:
-            gui_instance.append_message(f"Échec de la conversion de {file_name} en {image_format}. Erreur : {e}")
+            gui_instance.append_message(f"Failed to convert {file_name} to {image_format}. Error: {e}")
 
 def clean_metadata(image_paths, gui_instance):
     for image_path in image_paths:
